5fa3225a7f53fccab94e0389d175406cc9dcdc26
Tony <diyorbeksolijonov27@gmail.com>
2025-09-15 13:55:35 +0900

    feat develop member schema model

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,4 +1,4 @@
-import { Mutation, Resolver } from '@nestjs/graphql';
+import { Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 
 @Resolver()
@@ -23,9 +23,11 @@ export class MemberResolver {
         return this.memberService.updateMember();
     }
 
-  @Mutation(()=> String)
+  @Query(()=> String)
     public async getMember(): Promise<string>{
         console.log("Mutation: getMember");
         return this.memberService.getMember();
-    }}
+    }
+  
+  }
 
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class MemberService {}
+export class MemberService {
+
+    constructor (@InjectModel('Member')private readonly memberModel: Model<null>){}
+    public async signup(): Promise<string> {
+        return 'signup excuted'
+    }
+
+     public async login(): Promise<string> {
+        return 'login excuted'
+    }
+
+     public async updateMember(): Promise<string> {
+        return 'updateMember excuted'
+    }
+
+     public async getMember(): Promise<string> {
+        return 'getMember excuted'
+    }
+}
