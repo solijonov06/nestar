54634057dd12d32bc4c290714f5c5f175595cc06
Tony <diyorbeksolijonov27@gmail.com>
2025-09-25 00:34:39 +0900

    fix: modify updateMember graphql api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -9,6 +9,7 @@ import { ObjectId } from 'mongoose';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
 @Resolver()
 export class MemberResolver {
@@ -43,11 +44,13 @@ export class MemberResolver {
     }
 //authentication
   @UseGuards(AuthGuard)
-  @Mutation(()=> String)
-    public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string>{
+  @Mutation(()=> Member)
+    public async updateMember(@Args('input')input : MemberUpdate,
+     @AuthMember('_id') memberId: ObjectId): Promise<Member>{
         console.log("Mutation: updateMember");
         console.log(memberId)
-        return this.memberService.updateMember();
+        delete input._id
+        return this.memberService.updateMember(memberId, input);
     }
 
 
