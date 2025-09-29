6b31c8b62ba754dc871def570135cd80bc6448c9
Tony <diyorbeksolijonov27@gmail.com>
2025-09-29 13:45:02 +0900

    feat: develop getAllPropertiesByAdmin graphql logic

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,7 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
 import { Properties, Property } from '../../libs/dto/property/property';
-import { AgentPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, AllPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { UseGuards } from '@nestjs/common';
 import { MemberType } from '../../libs/enums/member.enum';
@@ -71,6 +71,15 @@ export class PropertyResolver {
             console.log("memberId", memberId)
             return await this.propertyService.getAgentProperties(memberId, input);
         }
+
+        @Roles(MemberType.ADMIN)
+            @UseGuards(RolesGuard)
+            @Query(() => Properties)
+            public async getAllPropertiesByAdmin(@Args("input") input: AllPropertiesInquiry,
+        @AuthMember('_id') memberId: ObjectId ): Promise<Properties> {
+                console.log('Query: getAllPropertiesByAdmin');
+                return await this.propertyService.getAllPropertiesByAdmin(input);
+            }
 
 }
  
--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -2,7 +2,7 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
 import { Direction, Message } from '../../libs/enums/common.enum';
-import { AgentPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, AllPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { Properties, Property } from '../../libs/dto/property/property';
 import { MemberService } from '../member/member.service';
 import { ViewService } from '../view/view.service';
@@ -188,6 +188,33 @@ export class PropertyService {
             if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND)
             return result[0];
         }
+
+
+        public async getAllPropertiesByAdmin(input: AllPropertiesInquiry): Promise<Properties> {
+                const { propertyStatus, propertyLocationList } = input.search
+                const match: T = {}
+                const sort: T = { [input?.sort ?? "createdAt"]: input?.direction ?? Direction.DESC }
+        
+                if (propertyStatus) match.propertyStatus = propertyStatus
+                if (propertyLocationList) match.propertyLocation = {$in: {propertyLocationList}}
+
+                const result = await this.propertyModel.aggregate([
+                    { $match: match },
+                    { $sort: sort },
+                    {
+                        $facet: {
+                            list: [{ $skip: (input.page - 1) * input.limit }, 
+                                { $limit: input.limit },//property1, property2
+                            lookupMember,//[memberValue]
+                          {$unwind: '$memberData'}//memberValue
+                        ],
+                            metaCounter: [{ $count: "total" }]
+                        }
+                    }
+                ]).exec()
+                if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND)
+                return result[0];
+            }
                 
             
     }
