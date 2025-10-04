9f4c0c9b14d90e4ff00e1e532ccd1c361927b9fa
Tony <diyorbeksolijonov27@gmail.com>
2025-10-04 18:26:27 +0900

    feat: develop likeTargetBoardArticle  graphql api

--- apps/nestar-api/src/components/board-article/board-article.resolver.ts
+++ apps/nestar-api/src/components/board-article/board-article.resolver.ts
@@ -63,6 +63,16 @@ export class BoardArticleResolver {
       return await this.boardArticleService.getBoardArticles(memberId, input);
    }
 
+   @UseGuards(WithoutGuard)
+   @Mutation(() => BoardArticle)
+   public async likeTargetBoardArticle(@Args("articleId") input: string, 
+   @AuthMember('_id') memberId: ObjectId): Promise<BoardArticle> {
+      console.log('Query: likeTargetBoardArticle');
+      const likeRefId = shapeIntoMongoObjectId(input)
+      return await this.boardArticleService.likeTargetBoardArticle(memberId, likeRefId)
+   }
+
+
    /** ADMIN */
 
    @Roles(MemberType.ADMIN)
